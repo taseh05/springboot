cba743e6d7110808e5f539844b64181fd2e55667
Tahaseen <taseh05@gmail.com>
2022-01-01 20:27:13 +0530

    initial commit

---
+++ homeloan-calculator/src/main/java/tahaseen/learning/homeloancalculator/HomeloanCalculatorApplication.java
@@ -0,0 +1,13 @@
+package tahaseen.learning.homeloancalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeloanCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeloanCalculatorApplication.class, args);
+	}
+
+}
---
+++ homeloan-calculator/src/test/java/tahaseen/learning/homeloancalculator/HomeloanCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package tahaseen.learning.homeloancalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeloanCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
